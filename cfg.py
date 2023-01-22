7db3def09ee3e4a79d5943f8ed6d5868f3956950
Timoxa2077 <123326656+Timoxa2077@users.noreply.github.com>
2023-01-23 00:05:26 +0300

    Delete cfg.py

--- cfg.py
+++
@@ -1,9 +0,0 @@
-import torchvision
-input_path = "./new_test/"
-output_path = ""
-output_name = "sub.csv"
-device = "none"
-return_file_name = False
-
-model = torchvision.models.vgg19
-model_wigth = "./wigths/vgg19_MSE.pth"
